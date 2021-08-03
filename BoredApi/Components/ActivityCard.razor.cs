04d2b8f40dde25c3efa00e55e0d8b07f72ce918b
Robin Landerloos <50279108+RobinLanderloos@users.noreply.github.com>
2021-08-03 21:26:13 +0200

    Moved code into their own components and added BlazorAnimate for animations

---
+++ BoredApi/Components/ActivityCard.razor.cs
@@ -0,0 +1,27 @@
+﻿using BoredApi.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoredApi.Components
+{
+    public partial class ActivityCard
+    {
+        [Parameter]
+        public ActivityResponse Activity { get; set; }
+
+        private string GenerateHowToLink(string activity)
+        {
+            if (!string.IsNullOrEmpty(activity))
+            {
+                var howToQuery = "How to " + Activity.Activity;
+                return "https://www.google.com/search?q=" + howToQuery.Replace(' ', '+');
+            }
+
+            return "https://www.google.com";
+        }
+    }
+}
